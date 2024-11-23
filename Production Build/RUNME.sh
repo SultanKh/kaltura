a38553ce5a87cd50e20c3c0eb0c09b72ab7ed837
Sultan <sultan.kh30@gmail.com>
2024-11-24 01:04:28 +0200

    Adding Production build
    How To Run:
    pre-requeste : developer should have npm & node installed on his system
    
    > cd Production Build
    > chmod +x RUNME.sh
    > RUNME.sh

---
+++ Production Build/RUNME.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+# MAKE sure to give permission: chmod +x RUNME.sh
+
+# Install live-server globally
+npm install -g live-server
+
+# Start live-server in the current directory
+live-server
+
+
